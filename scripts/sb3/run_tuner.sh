d4510eaf77ac637d1d6d02882775ecd8c3f8e499
semanur <semanur@PC.home>
2026-03-29 00:04:16 +0300

    optuna custom pruner added

--- scripts/sb3/run_tuner.sh
+++ scripts/sb3/run_tuner.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-for i in $(seq 1 500)
+for i in $(seq 1 5000)
 do
    echo "Starting Trial $i of 500"
    # Run the script. It will run 1 trial and exit.
-   python tuner2.py --headless
+   python tuner3.py --headless
    
    # Optional: sleep for a few seconds to let the GPU cool down
    sleep 2
